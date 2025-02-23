5303add0a8299d1e58680bfb97c4b1a2f0a5cb9d
Igor <igor.olshanskyi@gmail.com>
2025-02-23 22:54:29 +0200

    test_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import cn from 'classnames';
 import './App.scss';
+import { Person } from './types/Person';
 import { peopleFromServer } from './data/people';
+import debounce from 'lodash.debounce';
 
-export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+type Props = {
+  delay?: number;
+  onSelected?: (person: Person | null) => void;
+};
+
+export const App: React.FC<Props> = ({
+  delay = 300,
+  // onSelected = () => {},
+}) => {
+  const [targetPerson, setTargetPerson] = useState<Person | null>();
+  const [currentTodos, setCurrentTodos] = useState(peopleFromServer);
+  const [inputSort, setInputSort] = useState('');
+  const applyQuery = useCallback(debounce(setInputSort, delay), [delay]);
+
+  useEffect(() => {
+    return () => {
+      applyQuery.cancel();
+    };
+  }, [applyQuery]);
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputSort(event.target.value);
+    setCurrentTodos(
+      currentTodos.filter(person =>
+        person.name.toLocaleLowerCase().includes(inputSort.toLocaleLowerCase()),
+      ),
+    );
+  };
+
+  const todos = currentTodos.map(item => (
+    <div
+      className="dropdown-item"
+      data-cy="suggestion-item"
+      key={item.name}
+      onClick={() => setTargetPerson(item)}
+    >
+      <p className={cn('has-text-link')}>({item.name})</p>
+    </div>
+  ));
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {targetPerson &&
+            `${targetPerson?.name} (${targetPerson?.born} - ${targetPerson?.died})`}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,39 +60,12 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              onChange={handleChangeInput}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+            <div className="dropdown-content"> {todos} </div>
           </div>
         </div>
 
